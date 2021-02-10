b70f9c853fa16bd70f9a5e35a7c8bbbae28fe443
Ruslan Khabibulin <pcatrina@student.21-school.ru>
2021-02-10 10:58:13 +0300

    ex 2 finish

--- Module06/ex01/Serialization.cpp
+++ Module06/ex01/Serialization.cpp
@@ -1,62 +1,56 @@
 #include <iostream>
-#include <fstream>
 
-class data
+struct Data
 {
-public:
-	void setS1(const std::string &s1)
-	{
-		data::s1 = s1;
-	}
-
-	void setN(int n)
-	{
-		data::n = n;
-	}
-
-	void setS2(const std::string &s2)
-	{
-		data::s2 = s2;
-	}
-
-	const std::string &getS1() const
-	{
-		return s1;
-	}
-
-	int getN() const
-	{
-		return n;
-	}
-
-	const std::string &getS2() const
-	{
-		return s2;
-	}
-private:
 	std::string s1;
 	int n;
 	std::string s2;
 };
 
-//void *serialize(void)
-//{
-//
-//}
+std::string random(std::string str)
+{
+	char tmp[5];
+	for (int i = 0; i < 4; i++)
+	{
+		int j = rand() % (126 - 30) + 30;
+		tmp[i] = static_cast<char>(j);
+	}
+	tmp[4] = '\0';
+	str = std::string(tmp);
+	return str;
+}
+
+void *serialize(void)
+{
+	Data *m = new Data;
+	m->s1 = random(m->s1);
+	m->s2 = random(m->s2);
+	m->n = rand();
+
+	return (reinterpret_cast<void *>(m));
+}
+
+Data * deserialize(void * raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
 
 
 int main()
 {
-	data *m = new data;
 	srand(time(nullptr));
 
-	m->setS1("a4a4a4a4");
-	m->setN(rand());
-	m->setS2("aasd1997");
+	void *ptr;
+	Data *m;
 
-	std::cout << m->getS1().size() * sizeof(std::string::value_type)<<std::endl;
-	std::cout<< sizeof(m->getN())<<std::endl;
-	std::cout << m->getS2().size() * sizeof(std::string::value_type)<<std::endl;
+	ptr = serialize();
+	m = deserialize(ptr);
 
+
+	std::cout<<m->s1<<" " <<m->n<<" "<<m->s2<<std::endl;
+	std::cout<<sizeof(m->s1)<<" "<< sizeof(m->n)<<" "<< sizeof(m->s2)<<std::endl;
+	std::cout<< sizeof(Data)<<std::endl;
+
+	delete m;
 	return 0;
 }
